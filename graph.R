abf9ce91054a425decdd4087e04fec4bc2239605
Andreas Brandmaier <andy@brandmaier.de>
2024-03-01 11:50:26 +0100

    created initial logic for plot

--- graph.R
+++ graph.R
@@ -1,6 +1,27 @@
 library(gh)
+query <- gh("GET /repos/{username}/brandmaier/commits",
+               username = "brandmaier", per_page=100)
 
-print("generate something!")
+dates <- sapply(query, function(x) {
 
-my_repos <- gh("GET /users/{username}/repos", username = "brandmaier")
-print(my_repos)
+  date_string <- x$commit$committer$date
+  
+  date_time <- as.POSIXct(date_string, format = "%Y-%m-%dT%H:%M:%SZ")
+  
+  # Get the day of the week
+  day_of_week <- weekdays(date_time)
+  
+})
+
+library(dplyr)
+library(ggplot2)
+
+all_weekdays <- weekdays(as.POSIXct(paste0("1972-10-0",2:8," GMT")))
+
+dates <- ordered(dates, levels=all_weekdays)
+
+#data.frame(weekday=names(table(dates)),frequency=table(dates))
+plt <- tibble(dates)%>% 
+  ggplot(aes(x=dates,fill=dates))+geom_bar(stat="count")
+
+ggsave("weekdays.png", plot = plt, width = 300, height = 150)
